2943c0d68ba4c537240c6e5a839b44db1a7c95e1
aayushRedHat <aaayush@redhat.com>
2024-01-16 09:43:53 +0530

    check

--- test/test-project/test.sh
+++ test/test-project/test.sh
@@ -5,6 +5,7 @@ sudo chown -R 1001:121 "/root/.npm"
 cd app
 npm install
 cd test/test-project
+PUPPETEER_SKIP_DOWNLOAD=true npm install @asyncapi/html-template@0.16.0
 npm install
 npm run test:project
 #installing html template globally before global tests
@@ -13,3 +14,4 @@ PUPPETEER_SKIP_DOWNLOAD=true npm install -g @asyncapi/html-template@0.16.0
 rm -rf node_modules/@asyncapi/html-template
 rm -rf ../../node_modules/@asyncapi/html-template
 npm run test:global
+npm run test:registry
